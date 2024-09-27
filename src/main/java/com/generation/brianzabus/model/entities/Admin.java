9f60a0e457a1681db9ace10cbb0f229f72c0e04f
Difroy <unifroy@gmail.com>
2024-09-27 09:46:02 +0200

    0.0.2

--- src/main/java/com/generation/brianzabus/model/entities/Admin.java
+++ src/main/java/com/generation/brianzabus/model/entities/Admin.java
@@ -3,15 +3,46 @@ package com.generation.brianzabus.model.entities;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Admin 
 {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
 	int id;
 	String email, password;
 	LocalDate lastPasswordChange;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public LocalDate getLastPasswordChange() {
+		return lastPasswordChange;
+	}
+	public void setLastPasswordChange(LocalDate lastPasswordChange) {
+		this.lastPasswordChange = lastPasswordChange;
+	}
 
+	
+	
 	
 	
 	
--- src/main/java/com/generation/brianzabus/model/entities/Validable.java
+++ src/main/java/com/generation/brianzabus/model/entities/Validable.java
@@ -1,5 +1,7 @@
 package com.generation.brianzabus.model.entities;
 
+import java.util.List;
+
 /**
  *Questa interfaccia serve per segnalare che una classe è valida.
  *e per avere metodi concreti e infatti ora ne usufruirò
@@ -8,4 +10,25 @@ package com.generation.brianzabus.model.entities;
 
 public interface Validable {
 
+	
+	
+	/**
+	 * Questo metodo restituisce una lista di stringhe che rappresentano gli errori di validazione.
+	 * 
+	 * @return
+	 */
+	List<String> getErrors();
+	
+	
+	/**
+	 * Questo metodo restituisce true se l'oggetto è valido, false altrimenti.
+	 * 
+	 * @return
+	 */
+	default boolean isValid() {
+		return getErrors().isEmpty();
+	}
+	
+	
+	
 }
