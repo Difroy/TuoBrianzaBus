e7d2cfb8e16bdfc29e89a0e2bf111bba5959a1c4
Difroy <unifroy@gmail.com>
2024-09-27 10:10:16 +0200

    Update Admin.java

--- src/main/java/com/generation/brianzabus/model/entities/Admin.java
+++ src/main/java/com/generation/brianzabus/model/entities/Admin.java
@@ -1,6 +1,8 @@
 package com.generation.brianzabus.model.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -41,7 +43,44 @@ public class Admin
 		this.lastPasswordChange = lastPasswordChange;
 	}
 
+	public List<String> getErrors()
+	{
+		
+		
+		List<String> errors = new ArrayList<String>();
+		
+		if (email == null || email.isEmpty() || !email.contains("@") || !email.contains("."))
+			errors.add("Email is required");
+		if (email.matches("AZaz09._-!?"))
+			errors.add("Email is not valid");
 	
+	
+		if (password == null || password.isEmpty());
+			errors.add("Password is required");
+			if (lastPasswordChange == null)
+				errors.add("Last password change is required");
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//deve avere email e l'email deve essere valida
+		/*deve avere lastPasswordChange
+		 * la password deve contenere maiuscolo e minuscolo
+		 * minimo 6 caratteri, massimo 15 
+		 * deve conteneri almeno un numero
+		 * deve contenere almeno un carattere speciale fra .!?_-
+		 * 
+		 * 
+		 * */
+	}
 	
 	
 	
